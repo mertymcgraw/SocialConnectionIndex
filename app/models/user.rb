7b1c461fbae25806225f1b74bc9ea3d4d476024f
Merty McGraw <mertymcgraw@gmail.com>
2017-10-03 14:45:37 -0700

    add presence and numericality validations for user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, :linkedin_connections, :facebook_friends, :twitter_followers {presence: true}
+  validates :linkedin_connections, :facebook_friends, :twitter_followers, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
+  def social_connection_index
+
+  end 
+
 end
