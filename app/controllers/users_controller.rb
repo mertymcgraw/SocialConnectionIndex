2d39921ab32f325636fbea97014eb2d8a36a104e
Merty McGraw <mertymcgraw@gmail.com>
2017-10-03 15:27:15 -0700

    update users/index to display names and social_connection_index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def index
-    users = User.all
-    p users.last
-    p users.last.social_connection_index
+    @users = User.all
+    p @users.last
+    p @users.last.social_connection_index
 
   end 
 end
