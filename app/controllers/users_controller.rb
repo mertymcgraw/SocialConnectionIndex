1aa3dfd728da9d2a3b425fdfa30d3783d2e4325f
Merty McGraw <mertymcgraw@gmail.com>
2017-10-05 21:42:13 -0700

    Remove debugging statements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,6 @@ require 'will_paginate/array'
 class UsersController < ApplicationController
   def index
     @users = User.sort_by_social_connection_index.paginate(:page => params[:page], :per_page => 5)
-    
-    p "********"
-    p params
-    p params[:filter]
-    p "********"
 
     if params[:filter]
       @users = User.sorted_by(params[:filter]).paginate(:page => params[:page], :per_page => 5)
