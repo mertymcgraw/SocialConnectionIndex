d6ab31908fb91cbbe0a64be1285b4b45e32436f2
Merty McGraw <mertymcgraw@gmail.com>
2017-10-05 21:18:04 -0700

    Add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,26 @@
+require 'will_paginate/array'
+
 class UsersController < ApplicationController
   def index
-    @users = User.sort_by_social_connection_index
+    @users = User.sort_by_social_connection_index.paginate(:page => params[:page], :per_page => 5)
+    
     p "********"
     p params
+    p params[:filter]
     p "********"
 
     if params[:filter]
-      @users = User.sorted_by(params[:filter])
+      @users = User.sorted_by(params[:filter]).paginate(:page => params[:page], :per_page => 5)
     end 
 
     if params[:search]
-      @users = User.search(params[:search]).order("name ASC") 
+      @users = User.search(params[:search]).order("name ASC").paginate(:page => params[:page], :per_page => 5)
     end
 
     respond_to do |format|
       format.html {}
       format.js {}
     end 
-   
 
   end 
 
